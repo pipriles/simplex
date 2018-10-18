d5608782f2eec373d1bb4a34c603c569d50258b4
paharo <potopelao2@gmail.com>
2018-10-18 19:30:52 -0400

    SU MAINEO

--- main.c
+++ main.c
@@ -4,9 +4,28 @@
 
 int main() {
 
-	Matrix G;
+	Matrix G, C, b;
 
 	initMatrix(&G, 6, 4);
+	initMatrix(&C, 1, 6);
+	initMatrix(&b, 4, 1);
+
+	setAt(&C, 0, 0, 5);
+	setAt(&C, 0, 1, 4);
+	setAt(&C, 0, 2, 0);
+	setAt(&C, 0, 3, 0);
+	setAt(&C, 0, 4, 0);
+	setAt(&C, 0, 5, 0);
+
+	setAt(&b, 0, 0, 24);
+	setAt(&b, 1, 0, 6);
+	setAt(&b, 2, 0, 1);
+	setAt(&b, 3, 0, 2);
+
+	setAt(&b, 0, 0, 24);
+	setAt(&b, 0, 1, 6);
+	setAt(&b, 0, 2, 1);
+	setAt(&b, 0, 3, 2);
 
 	setAt(&G, 0, 0, 1);
 	setAt(&G, 0, 1, 0);
