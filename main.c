2e7d6fc4b29e2bc47c2183936778782bb63378c9
GshIT HUB <alabbe@uc.edu.ve>
2018-10-18 21:51:52 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "matrix.h"
+#include "simplex.h"
+
+int main() {
+
+	Matrix G, C, b;
+
+	initMatrix(&G, 4, 2);
+	initMatrix(&C, 1, 2);
+	initMatrix(&b, 4, 1);
+
+	setAt(&b, 0, 0, 24);
+	setAt(&b, 1, 0, 6);
+	setAt(&b, 2, 0, 1);
+	setAt(&b, 3, 0, 2);
+
+	setAt(&b, 0, 0, 24);
+	setAt(&b, 0, 1, 6);
+	setAt(&b, 0, 2, 1);
+	setAt(&b, 0, 3, 2);
+
+	setAt(&G, 0, 0, 6);
+	setAt(&G, 1, 0, 1);
+	setAt(&G, 2, 0, -1);
+	setAt(&G, 3, 0, 0);
+
+	setAt(&G, 0, 1, 4);
+	setAt(&G, 1, 1, 2);
+	setAt(&G, 2, 1, 1);
+	setAt(&G, 3, 1, 1);
+
+	setAt(&C, 0, 0, 5);
+	setAt(&C, 0, 1, 4);
+
+	simplex(G,C,b);
+
+	freeMatrix(&G);
+	freeMatrix(&C);
+	freeMatrix(&b);
+
+	return 0;
+}
---
+++ simplex.h
@@ -0,0 +1,15 @@
+#ifndef _SIMPLEX_H_
+#define _SIMPLEX_H_
+#include "matrix.h"
+
+Matrix B, Cb;
+
+void initialize(Matrix G);
+size_t optimality(Matrix NB, Matrix Binv, Matrix Cnb);
+Matrix loadIdentity(size_t s);
+void simplex(Matrix NB,Matrix Cnb,Matrix b);
+void simplexEnd();
+size_t minimum(MTYPE *array, size_t n);
+
+#endif
+
